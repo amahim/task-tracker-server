b75df89b06a62b804f0076ee3c3f133e14abbc6d
amahim <arifbin71@gmail.com>
2025-02-20 01:18:10 +0600

    user db

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kreq4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
 async function run() {
@@ -28,23 +29,29 @@ async function run() {
     await client.connect();
 
     const usersCollection = client.db("taskTracker").collection("users");
-    
-   
 
+    // !User related APIs--------------------------------------------
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return;
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-
-
-
-
-
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,10 +59,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+
 app.get("/", (req, res) => {
   res.send("task-tracker server is running");
-});
-
+});  // Add this closing bracket
 app.listen(port, () => {
   console.log(`task-tracker server is running on port ${port}`);
 });
